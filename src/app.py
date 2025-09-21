bd2b76f72a2a8e3c77806108f9c67c99cd33ae4e
Ale Heir <aheir@itba.edu.ar>
2025-09-21 04:13:48 -0300

    Remove qt-material dependency and related arguments for theme management bc changed UI ratios . Pending finding a replacement.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 # PyQt5 modules
 from PyQt6 import QtWidgets
-from qt_material import apply_stylesheet
 import logging
 import argparse
 
@@ -19,21 +18,12 @@ def main():
         default='ERROR',
         help='Set the logging level (default: ERROR)'
     )
-    parser.add_argument(
-        '--theme', '-t',
-        choices=['light', 'dark'],
-        default='light',
-        help='Set the UI theme (default: light)'
-    )
     args = parser.parse_args()
     
     logging.basicConfig(level=getattr(logging, args.log_level.upper()), format='%(levelname)s: %(message)s')
    
     app = QtWidgets.QApplication(sys.argv)
 
-    theme_file = 'dark_blue.xml' if args.theme == 'dark' else 'light_blue.xml'
-    apply_stylesheet(app, theme=theme_file)
-    
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
