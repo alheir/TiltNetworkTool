9451c4309190d5679706eb46b40963c9ad4bfe3e
Ale Heir <aheir@itba.edu.ar>
2025-09-21 16:25:08 -0300

    Adjust time axis in PlotWidget to start from zero and enhance grid visibility

--- src/widgets/plot_widget.py
+++ src/widgets/plot_widget.py
@@ -2,12 +2,14 @@ import pyqtgraph as pg
 from PyQt6.QtWidgets import QWidget, QVBoxLayout
 import PyQt6.QtCore as QtCore
 import numpy as np
+import time
 
 class PlotWidget(QWidget):
     def __init__(self, station_index, angle_histories):
         super().__init__()
         self.station_index = station_index
         self.angle_histories = angle_histories  # [roll_history, pitch_history, yaw_history], cada una con [(timestamp, value), ...]
+        self.start_time = time.time()
         self.setWindowTitle(f"Station {station_index} Angle Plot")
         self.setGeometry(100, 100, 600, 300)
 
@@ -18,6 +20,7 @@ class PlotWidget(QWidget):
         self.plot_widget.setLabel('bottom', 'Time (s)')
         self.plot_widget.setTitle(f'Station {station_index} Angles Over Time')
         self.plot_widget.addLegend()
+        self.plot_widget.showGrid(x=True, y=True, alpha=0.3)
 
         self.curves = []
         colors = ['r', 'g', 'b']
@@ -45,6 +48,7 @@ class PlotWidget(QWidget):
         for i, (history, curve) in enumerate(zip(self.angle_histories, self.curves)):
             if history:
                 times, values = zip(*history)
-                curve.setData(x=np.array(times), y=np.array(values))
+                relative_times = np.array(times) - self.start_time # Para empezar en t=0
+                curve.setData(x=relative_times, y=np.array(values))
             else:
                 curve.setData(x=[], y=[])
